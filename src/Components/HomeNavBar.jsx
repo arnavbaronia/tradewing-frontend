579838593e59803f04f8b35758b11403824db6d1
arnavbaronia <arnav.baronia@gmail.com>
2025-03-25 17:59:45 +0530

    navbar icons

--- src/Components/HomeNavBar.jsx
+++ src/Components/HomeNavBar.jsx
@@ -3,6 +3,14 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../Styles/HomeNavBar.css";
 import { FiMenu } from "react-icons/fi";
 import logo from "../assets/twlogotransparentt.png";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
+import CurrencyRupeeOutlinedIcon from '@mui/icons-material/CurrencyRupeeOutlined';
+import AddLocationAltOutlinedIcon from '@mui/icons-material/AddLocationAltOutlined';
+import RateReviewOutlinedIcon from '@mui/icons-material/RateReviewOutlined';
+import AddIcCallOutlinedIcon from '@mui/icons-material/AddIcCallOutlined';
+import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
 
 const HomeNavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -66,30 +74,35 @@ const HomeNavBar = () => {
           className={`nav-link ${isActive("/")}`}
           onClick={handleHomeClick}
         >
+          <HomeOutlinedIcon className="nav-icon" />
           Home
         </span>
         <span
           className={`nav-link ${isActive("/about")}`}
           onClick={() => handleSectionClick("about-us")}
         >
+          <Person2OutlinedIcon className="nav-icon" />
           About
         </span>
         <span
           className={`nav-link ${isActive("/courses")}`}
           onClick={() => handleSectionClick("courses")}
         >
+          <MenuBookOutlinedIcon className="nav-icon" />
           Courses
         </span>
         <span
           className={`nav-link ${isActive("/payments")}`}
           onClick={() => handleSectionClick("payments")}
         >
+          <CurrencyRupeeOutlinedIcon className="nav-icon" />
           Payments
         </span>
         <span
           className={`nav-link ${isActive("/presence")}`}
           onClick={() => handleSectionClick("our-presence")}
         >
+          <AddLocationAltOutlinedIcon className="nav-icon" />
           Our Presence
         </span>
         <Link
@@ -97,6 +110,7 @@ const HomeNavBar = () => {
           className={`nav-link ${isActive("/reviews")}`}
           onClick={handleLinkClick}
         >
+          <RateReviewOutlinedIcon className="nav-icon" />
           Reviews
         </Link>
         <Link
@@ -104,6 +118,7 @@ const HomeNavBar = () => {
           className={`nav-link ${isActive("/contact")}`}
           onClick={handleLinkClick}
         >
+          <AddIcCallOutlinedIcon className="nav-icon" />
           Contact Us
         </Link>
         <Link
@@ -111,6 +126,7 @@ const HomeNavBar = () => {
           className={`nav-link ${isActive("/refer")}`}
           onClick={handleLinkClick}
         >
+          <GroupAddOutlinedIcon className="nav-icon" />
           Refer a Friend
         </Link>
       </div>
